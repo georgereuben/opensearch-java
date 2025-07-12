f20c5bb54347d0fe740c50132f7b93249c7d43c8
georgereuben <reubengeorge101@gmail.com>
2025-07-12 21:04:15 +0530

    add ResponseHeaderExtractor for multivalue headers

--- java-client/src/main/java/org/opensearch/client/transport/aws/ResponseHeaderExtractor.java
+++ java-client/src/main/java/org/opensearch/client/transport/aws/ResponseHeaderExtractor.java
@@ -1,10 +1,9 @@
 package org.opensearch.client.transport.aws;
 
-import java.util.LinkedHashMap;
+import java.util.*;
+
 import software.amazon.awssdk.http.SdkHttpResponse;
 
-import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 public final class ResponseHeaderExtractor {
@@ -17,6 +16,27 @@ public final class ResponseHeaderExtractor {
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
                         entry -> Objects.toString(entry.getValue()),
+                        (existing, replacement) -> existing,
+                        LinkedHashMap::new
+                ));
+    }
+
+    public static Map<String, List<String>> extractMultiValueHeaders(SdkHttpResponse httpResponse) {
+        return httpResponse.headers()
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        entry -> {
+                            Object value = entry.getValue();
+                            if (value instanceof List) {
+                                return ((List<?>) value).stream()
+                                        .map(Objects::toString)
+                                        .collect(Collectors.toList());
+                            } else {
+                                return Collections.singletonList(Objects.toString(value));
+                            }
+                        },
                         (existing, replacement) -> existing,
                         LinkedHashMap::new
                 ));
