8922fa8d26cade7e801ea638966bddfdf7a6862c
georgereuben <reubengeorge101@gmail.com>
2025-07-16 12:48:32 +0530

    add ResponseProxyHandler for handling getResponseHeaders calls

---
+++ java-client/src/main/java/org/opensearch/client/transport/aws/ResponseProxyHandler.java
@@ -0,0 +1,26 @@
+package org.opensearch.client.transport.aws;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.Map;
+
+public class ResponseProxyHandler implements InvocationHandler {
+    private final Object originalResponse;
+    private final Map<String, String> responseHeaders;
+
+    public ResponseProxyHandler(Object originalResponse, Map<String, String> responseHeaders) {
+        this.originalResponse = originalResponse;
+        this.responseHeaders = Collections.unmodifiableMap(responseHeaders);
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if("getResponseHeaders".equals(method.getName()) && method.getParameterCount() == 0) {
+            return responseHeaders;
+        }
+
+        // sending all other calls to the original response object
+        return method.invoke(originalResponse, args);
+    }
+}
